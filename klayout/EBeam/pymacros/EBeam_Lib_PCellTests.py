e60e435b3dbd31ba4df12fc3d12ed7a2736eaaf2
Fariha Ibnat Shaikh <fariha@lukas4.ece.ubc.ca>
2023-11-02 10:43:40 -0700

    Inserting custom_exceptions class inside test script to debug

--- klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
+++ klayout/EBeam/pymacros/EBeam_Lib_PCellTests.py
@@ -7,8 +7,32 @@ import pathlib
 #import pcells_EBeam ### folder name ###
 import importlib
 
-from custom_exceptions import *
+class PCellRegistrationError(Exception):
+    """
+    Raised when a PCell is not properly registered in a library. This occurs when 
+    a PCell's .py file is in a library's folder but has not been registered in the library layout
+    """
+    def __init__(self, pcell, library):
+        self.pcell = pcell
+        self.library = library
+        super().__init__("Pcell {} could not be registered in library {}".format(pcell, library))
 
+class PCellInstantiationError(Exception):
+    """
+    Raised when a PCell is empty (not containing instances nor any shapes) when instantiated in a 
+    new layout
+    """
+    def __init__(self, pcell, library):
+        self.pcell = pcell
+        super().__init__("Pcell {} from library {} is empty when instantiated in a new layout".format(pcell, library))
+
+class LibraryNotRegistered(Exception):
+    """
+    Raised when a library is not registered in Klayout
+    """
+    def __init__(self, library):
+        self.library = library
+        super().__init__("Library {} is not registered in Klayout")
 
 """
 Python script to test that all Pcells are properly registered in their respective library and will display polygons when placed on a new layout.
